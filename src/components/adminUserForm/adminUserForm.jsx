b272f9080880a462043c04b3d50ebf15d93a33f4
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-05-20 20:03:30 +0530

    correct TypoError

--- src/components/adminUserForm/adminUserForm.jsx
+++ src/components/adminUserForm/adminUserForm.jsx
@@ -9,7 +9,7 @@ const AdminUserForm = () => {
 
   return (
     <form action={formAction} className={styles.container}>
-      <h1>New Post</h1>
+      <h1>Add New user</h1>
       <input type="text" name="username" placeholder="username" />
       <input type="text" name="email" placeholder="Email" />
       <input type="text" name="img" placeholder="Upload Your Profile Image URL" />
